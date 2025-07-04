f2aa1f133e158c93c88b429e1af53d80ac77df28
CoPokBl <copokbl@serble.net>
2025-07-04 12:22:00 +1000

    bump minestom version (test impl)

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,7 @@ repositories {
 dependencies {
     compileOnly("net.minestom:minestom:2025.07.03-1.21.5")
 
-    testImplementation("net.minestom:minestom-snapshots:4fe2993057")
+    testImplementation("net.minestom:minestom:2025.07.03-1.21.5")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
